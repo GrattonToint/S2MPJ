ff98723f6863cf047f99cd4fb6681fa6105fb72a
phtoint <philippe.toint@unamur.be>
2024-05-31 18:13:07 +0200

    matlab R

---
+++ matlab_problems/RECIPELS.m
@@ -0,0 +1,219 @@
+function varargout = RECIPELS(action,varargin)
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% 
+% 
+%    Problem : RECIPELS
+%    *********
+% 
+%    Source:  problem 155 (p. 88) in
+%    A.R. Buckley,
+%    "Test functions for unconstrained minimization",
+%    TR 1989CS-3, Mathematics, statistics and computing centre,
+%    Dalhousie University, Halifax (CDN), 1989.
+% 
+%    SIF input: Ph. Toint, Dec 1989.
+%    Least-squares version of RECIPE.SIF, Nick Gould, Jan 2020.
+% 
+%    classification = 'SUR2-AY-3-0'
+% 
+% 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+persistent pbm;
+
+name = 'RECIPELS';
+
+switch(action)
+
+    case 'setup'
+
+    pb.name      = 'RECIPELS';
+    pb.sifpbname = 'RECIPELS';
+    pbm.name     = 'RECIPELS';
+        %%%%%%%%%%%%%%%%%%%%  PREAMBLE %%%%%%%%%%%%%%%%%%%%
+        v_  = configureDictionary('string','double');
+        ix_ = configureDictionary('string','double');
+        ig_ = configureDictionary('string','double');
+        %%%%%%%%%%%%%%%%%%%%  VARIABLES %%%%%%%%%%%%%%%%%%%%
+        pb.xnames = {};
+        [iv,ix_] = s2xlib('ii','X1',ix_);
+        pb.xnames{iv} = 'X1';
+        [iv,ix_] = s2xlib('ii','X2',ix_);
+        pb.xnames{iv} = 'X2';
+        [iv,ix_] = s2xlib('ii','X3',ix_);
+        pb.xnames{iv} = 'X3';
+        %%%%%%%%%%%%%%%%%%%  DATA GROUPS %%%%%%%%%%%%%%%%%%%
+        pbm.A = sparse(0,0);
+        [ig,ig_] = s2xlib('ii','G1',ig_);
+        gtype{ig} = '<>';
+        iv = ix_('X1');
+        if(size(pbm.A,1)>=ig&&size(pbm.A,2)>=iv)
+            pbm.A(ig,iv) = 1.0+pbm.A(ig,iv);
+        else
+            pbm.A(ig,iv) = 1.0;
+        end
+        [ig,ig_] = s2xlib('ii','G2',ig_);
+        gtype{ig} = '<>';
+        [ig,ig_] = s2xlib('ii','G3',ig_);
+        gtype{ig} = '<>';
+        %%%%%%%%%%%%%%% GLOBAL DIMENSIONS %%%%%%%%%%%%%%%%%
+        pb.n   = numEntries(ix_);
+        ngrp   = numEntries(ig_);
+        pbm.objgrps = [1:ngrp];
+        pb.m        = 0;
+        %%%%%%%%%%%%%%%%%%% CONSTANTS %%%%%%%%%%%%%%%%%%%%%
+        pbm.gconst = zeros(ngrp,1);
+        pbm.gconst(ig_('G1')) = 5.0;
+        pb.xlower = zeros(pb.n,1);
+        pb.xupper = +Inf*ones(pb.n,1);
+        %%%%%%%%%%%%%%%%%%%%  BOUNDS %%%%%%%%%%%%%%%%%%%%%
+        pb.xlower = -Inf*ones(pb.n,1);
+        pb.xupper = +Inf*ones(pb.n,1);
+        %%%%%%%%%%%%%%%%%%%% START POINT %%%%%%%%%%%%%%%%%%
+        pb.x0(1:pb.n,1) = zeros(pb.n,1);
+        pb.x0(ix_('X1'),1) = 2.0;
+        pb.x0(ix_('X2'),1) = 5.0;
+        pb.x0(ix_('X3'),1) = 1.0;
+        %%%%%%%%%%%%%%%%%%%%% ELFTYPE %%%%%%%%%%%%%%%%%%%%%
+        iet_ = configureDictionary('string','double');
+        [it,iet_] = s2xlib( 'ii', 'eSQ',iet_);
+        elftv{it}{1} = 'X';
+        [it,iet_] = s2xlib( 'ii', 'eXOVERU',iet_);
+        elftv{it}{1} = 'X';
+        elftv{it}{2} = 'Y';
+        elftv{it}{3} = 'Z';
+        %%%%%%%%%%%%%%%%%%% ELEMENT USES %%%%%%%%%%%%%%%%%%
+        ie_ = configureDictionary('string','double');
+        pbm.elftype = {};
+        ielftype    = [];
+        pbm.elvar   = {};
+        ename = 'E1';
+        [ie,ie_] = s2xlib('ii',ename,ie_);
+        pbm.elftype{ie} = 'eSQ';
+        ielftype(ie) = iet_('eSQ');
+        vname = 'X2';
+        [iv,ix_,pb] = s2xlib('nlx',vname,ix_,pb,1,[],[],[]);
+        posev = find(strcmp('X',elftv{ielftype(ie)}));
+        pbm.elvar{ie}(posev) = iv;
+        ename = 'E2';
+        [ie,ie_] = s2xlib('ii',ename,ie_);
+        pbm.elftype{ie} = 'eXOVERU';
+        ielftype(ie) = iet_('eXOVERU');
+        vname = 'X3';
+        [iv,ix_,pb] = s2xlib('nlx',vname,ix_,pb,1,[],[],[]);
+        posev = find(strcmp('X',elftv{ielftype(ie)}));
+        pbm.elvar{ie}(posev) = iv;
+        vname = 'X2';
+        [iv,ix_,pb] = s2xlib('nlx',vname,ix_,pb,1,[],[],[]);
+        posev = find(strcmp('Y',elftv{ielftype(ie)}));
+        pbm.elvar{ie}(posev) = iv;
+        vname = 'X1';
+        [iv,ix_,pb] = s2xlib('nlx',vname,ix_,pb,1,[],[],[]);
+        posev = find(strcmp('Z',elftv{ielftype(ie)}));
+        pbm.elvar{ie}(posev) = iv;
+        %%%%%%%%%%%%%%%%%%%%%% GRFTYPE %%%%%%%%%%%%%%%%%%%%
+        igt_ = configureDictionary('string','double');
+        [it,igt_] = s2xlib('ii','gL2',igt_);
+        %%%%%%%%%%%%%%%%%%%% GROUP USES %%%%%%%%%%%%%%%%%%%
+        [pbm.grelt{1:ngrp}] = deal(repmat([],1,ngrp));
+        nlc = [];
+        for ig = 1:ngrp
+            pbm.grftype{ig} = 'gL2';
+        end
+        ig = ig_('G2');
+        posel = length(pbm.grelt{ig})+1;
+        pbm.grelt{ig}(posel) = ie_('E1');
+        pbm.grelw{ig}(posel) = 1.;
+        ig = ig_('G3');
+        posel = length(pbm.grelt{ig})+1;
+        pbm.grelt{ig}(posel) = ie_('E2');
+        pbm.grelw{ig}(posel) = 1.;
+        %%%%%%%%% DEFAULT FOR MISSING SECTION(S) %%%%%%%%%%
+        %%%%%% RETURN VALUES FROM THE SETUP ACTION %%%%%%%%
+        pb.pbclass = 'SUR2-AY-3-0';
+        varargout{1} = pb;
+        varargout{2} = pbm;
+
+    %%%%%%%%%%%%%%%% NONLINEAR ELEMENTS %%%%%%%%%%%%%%%
+
+    case 'eSQ'
+
+        EV_  = varargin{1};
+        iel_ = varargin{2};
+        varargout{1} = EV_(1)*EV_(1);
+        if(nargout>1)
+            g_(1,1) = EV_(1)+EV_(1);
+            varargout{2} = g_;
+            if(nargout>2)
+                H_(1,1) = 2.0;
+                varargout{3} = H_;
+            end
+        end
+
+    case 'eXOVERU'
+
+        EV_  = varargin{1};
+        iel_ = varargin{2};
+        U_ = zeros(2,3);
+        U_(1,1) = U_(1,1)+1;
+        U_(2,2) = U_(2,2)+1;
+        U_(2,3) = U_(2,3)-1;
+        IV_(1) = U_(1,:)*EV_;
+        IV_(2) = U_(2,:)*EV_;
+        U2 = IV_(2)*IV_(2);
+        varargout{1} = IV_(1)/IV_(2);
+        if(nargout>1)
+            g_(1,1) = 1.0/IV_(2);
+            g_(2,1) = -IV_(1)/U2;
+            varargout{2} = U_.'*g_;
+            if(nargout>2)
+                H_ = sparse(2,2);
+                H_(1,2) = -1.0/U2;
+                H_(2,1) = H_(1,2);
+                H_(2,2) = 2.0*IV_(1)/(U2*IV_(2));
+                varargout{3} = U_.'*H_*U_;
+            end
+        end
+
+    %%%%%%%%%%%%%%%%%% NONLINEAR GROUPS  %%%%%%%%%%%%%%%
+
+    case 'gL2'
+
+        GVAR_ = varargin{1};
+        igr_  = varargin{2};
+        varargout{1} = GVAR_*GVAR_;
+        if(nargout>1)
+            g_ = GVAR_+GVAR_;
+            varargout{2} = g_;
+            if(nargout>2)
+                H_ = 2.0;
+                varargout{3} = H_;
+            end
+        end
+
+    %%%%%%%%%%%%%%%% THE MAIN ACTIONS %%%%%%%%%%%%%%%
+
+    case {'fx','fgx','fgHx','cx','cJx','cJHx','cIx','cIJx','cIJHx','cIJxv','fHxv',...
+          'cJxv','Lxy','Lgxy','LgHxy','LIxy','LIgxy','LIgHxy','LHxyv','LIHxyv'}
+
+        if(isfield(pbm,'name')&&strcmp(pbm.name,name))
+            pbm.has_globs = [0,0];
+            [varargout{1:max(1,nargout)}] = s2xlib(action,pbm,varargin{:});
+        else
+            disp(['ERROR: please run ',name,' with action = setup'])
+        [varargout{1:nargout}] = deal(repmat(NaN,1:nargout));
+            end
+
+    otherwise
+        disp([' ERROR: unknown action ',action,' requested from ',name,'.m'])
+    end
+
+return
+
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
